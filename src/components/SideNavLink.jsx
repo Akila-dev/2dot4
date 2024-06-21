a57903e8042ed94ce2327a64e75e308aa3f86372
unknown <akilapanunayan@gmail.com>
2024-06-21 14:54:58 +0100

    init

--- src/components/SideNavLink.jsx
+++ src/components/SideNavLink.jsx
@@ -2,7 +2,7 @@
 const SideNavLink = ({ data, active, id, onClick }) => {
 	return (
 		<button
-			className="w-full h-full md:h-auto block uppercase text-xs md:text-base xl:text-[1.1rem] font-medium text-[--grey] hover:font-medium hover:text-[--white] pointer-events-auto text-left "
+			className="w-full h-full md:h-auto block uppercase text-xs md:text-base xl:text-[1.1rem] font-medium text-[--grey] hover:font-medium hover:text-[--white] pointer-events-auto text-left  text-nowrap"
 			onClick={onClick}
 		>
 			<span
