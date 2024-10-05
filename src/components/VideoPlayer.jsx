c1e5828332133b41e17232fe1ab684e86cc63002
unknown <akilapanunayan@gmail.com>
2024-10-05 15:49:30 +0100

    bug fixes

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -5,11 +5,16 @@ export default function VideoPlayer({ src }) {
 	const videoRef = useRef();
 	useEffect(() => {
 		videoRef.current.defaultMuted = true;
+		videoRef.current.defaultPlaybackRate = 1;
+		videoRef.current.controls = false;
+		videoRef.current.autoplay = true;
+		videoRef.current.loop = true;
+		console.log(videoRef);
 	});
 
 	return (
 		<video
-			className="w-full h-full object-cover"
+			className="w-full h-screen object-cover"
 			ref={videoRef}
 			loop
 			autoPlay
