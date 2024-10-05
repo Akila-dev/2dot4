7c28233cb24f987125fd5e4442d1b91e0294a0e6
unknown <akilapanunayan@gmail.com>
2024-10-05 15:40:11 +0100

    bug fixes

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,66 +1,22 @@
 /* eslint-disable react/prop-types */
-import { useRef, useState, useEffect } from 'react';
-
-const isSafari = () => {
-	const ua = navigator.userAgent.toLowerCase();
-	return ua.indexOf('safari') > -1 && ua.indexOf('chrome') < 0;
-};
+import { useRef, useEffect } from 'react';
 
 export default function VideoPlayer({ src }) {
-	const videoParentRef = useRef();
-	const [shouldUseImage, setShouldUseImage] = useState(false);
+	const videoRef = useRef();
 	useEffect(() => {
-		// check if user agent is safari and we have the ref to the container <div />
-		if (isSafari() && videoParentRef.current) {
-			// obtain reference to the video element
-			const player = videoParentRef.current.children[0];
+		videoRef.current.defaultMuted = true;
+	});
 
-			// if the reference to video player has been obtained
-			if (player) {
-				// set the video attributes using javascript as per the
-				// webkit Policy
-				player.controls = false;
-				player.playsinline = true;
-				player.muted = true;
-				player.setAttribute('muted', ''); // leave no stones unturned :)
-				player.autoplay = true;
-
-				// Let's wait for an event loop tick and be async.
-				setTimeout(() => {
-					// player.play() might return a promise but it's not guaranteed crossbrowser.
-					const promise = player.play();
-					// let's play safe to ensure that if we do have a promise
-					if (promise.then) {
-						promise
-							.then(() => {})
-							.catch(() => {
-								// if promise fails, hide the video and fallback to <img> tag
-								videoParentRef.current.style.display = 'none';
-								setShouldUseImage(true);
-							});
-					}
-				}, 0);
-			}
-		}
-	}, []);
-
-	return shouldUseImage ? (
-		<img src={src} alt="Muted Video" />
-	) : (
-		<div
-			ref={videoParentRef}
-			dangerouslySetInnerHTML={{
-				__html: `
-        <video
-          loop
-          muted
-          autoplay
-          playsinline
-          preload="metadata"
-        >
-        <source src="${src}" type="video/webm" />
-        </video>`,
-			}}
-		/>
+	return (
+		<video
+			className="w-full h-full object-cover"
+			ref={videoRef}
+			loop
+			autoPlay
+			muted
+			playsInline
+		>
+			<source src={src} type="video/mp4" />
+		</video>
 	);
 }
